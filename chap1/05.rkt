a92e774490946d7505b69d080bd55dd26dc18266
ramu13 <hidekiramu13@gmail.com>
2022-03-13 21:31:09 +0900

    initial implement: 05.rkt

--- chap1/05.rkt
+++ chap1/05.rkt
@@ -1,6 +1,17 @@
 #lang sicp
 
-;
-;
+;無限に再帰する関数
+;(p)という式が基本式になるまでをたどろうとする
+(define (p) (p))
 
-()
+
+(define (test x y)
+  (if (= x 0) 0 y))
+
+;test_case_1
+(test 0 (+ 0 1))
+; 0
+
+;test_case_2
+(test 0 (p))
+; (処理が終わらない)
